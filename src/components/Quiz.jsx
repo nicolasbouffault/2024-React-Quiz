50caf61f87994d9a0c08163aa421b4d3f366d50e
Nicolas Bouffault <nicolasbouffault@gmail.com>
2023-12-01 21:07:23 +0000

    Adding header, questions and answers

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import questions from "../questions";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{questions[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {questions[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
