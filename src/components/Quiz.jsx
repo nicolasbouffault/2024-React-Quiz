b69516e979bfe2cd8d83bd744e8bed53fe4ef292
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2024-01-19 21:00:48 +0000

    Create new question component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,69 +2,64 @@ import { useCallback, useState } from "react";
 
 import quizCompleteImg from "../assets/quiz-complete.png";
 import questions from "../questions";
-import QuestionTimer from "./QuestionTimer";
-import Answers from "./Answers.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
-  const [userAnswers, setUserAnswers] = useState([]);
+    const [answerState, setAnswerState] = useState("");
+    const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
-  const quizIsComplete = activeQuestionIndex === questions.length;
+    const activeQuestionIndex =
+        answerState === "" ? userAnswers.length : userAnswers.length - 1;
+    const quizIsComplete = activeQuestionIndex === questions.length;
 
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered");
-      setUserAnswers((prevUserAnswers) => {
-        return [...prevUserAnswers, selectedAnswer];
-      });
-      setTimeout(() => {
-        if (selectedAnswer === questions[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
+    const handleSelectAnswer = useCallback(
+        function handleSelectAnswer(selectedAnswer) {
+            setAnswerState("answered");
+            setUserAnswers((prevUserAnswers) => {
+                return [...prevUserAnswers, selectedAnswer];
+            });
+            setTimeout(() => {
+                if (
+                    selectedAnswer === questions[activeQuestionIndex].answers[0]
+                ) {
+                    setAnswerState("correct");
+                } else {
+                    setAnswerState("wrong");
+                }
 
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
-
-  const handleSkipAnswer = useCallback(
-    () => handleSelectAnswer(null),
-    [handleSelectAnswer]
-  );
-
-  if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy Icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
+                setTimeout(() => {
+                    setAnswerState("");
+                }, 2000);
+            }, 1000);
+        },
+        [activeQuestionIndex]
     );
-  }
 
-  return (
-    <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex}
-          timeout={10000}
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{questions[activeQuestionIndex].text}</h2>
-        <Answers
-          key={activeQuestionIndex}
-          answers={questions[activeQuestionIndex].answers}
-          selectedAnswer={userAnswers[userAnswers.length - 1]}
-          answerState={answerState}
-          onSelect={handleSelectAnswer}
-        />
-      </div>
-    </div>
-  );
+    const handleSkipAnswer = useCallback(
+        () => handleSelectAnswer(null),
+        [handleSelectAnswer]
+    );
+
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy Icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+
+    return (
+        <div id="quiz">
+            <Question
+                key={activeQuestionIndex}
+                questionText={questions[activeQuestionIndex].text}
+                answers={questions[activeQuestionIndex].answers}
+                onSelectAnswer={handleSelectAnswer}
+                answerState={answerState}
+                selectedAnswer={userAnswers[userAnswers.length - 1]}
+                onSkipAnswer={handleSkipAnswer}
+            />
+        </div>
+    );
 }
